e8369e7a107bd07989743da696374b92dd4222b1
Moises P. Sena <moisespsena@gmail.com>
2021-04-27 15:35:36 -0300

    feat: supports stack tracer

--- tracederror.go
+++ tracederror.go
@@ -1,8 +1,11 @@
 package tracederror
 
 import (
+	"bytes"
 	"fmt"
+	"runtime"
 	"runtime/debug"
+	"strings"
 
 	"github.com/pkg/errors"
 )
@@ -26,11 +29,86 @@ func badErrOrMsg() {
 	panic("bad errOrMsg type")
 }
 
+type SimpleStackTracer interface {
+	StackTrace() errors.StackTrace
+}
+
+type StackTracer interface {
+	TracedError
+	StackTrace() errors.StackTrace
+}
+
+type stackTraceError struct {
+	error
+	trace []byte
+}
+
+func (this stackTraceError) Cause() error {
+	return this.error
+}
+
+func stackTracerToBytes(t SimpleStackTracer) []byte {
+	b := make([]byte, 64)
+	b = b[:runtime.Stack(b, false)]
+	b = b[:bytes.IndexByte(b, '\n')+1]
+
+	var buf = bytes.NewBuffer(b)
+	for _, f := range t.StackTrace() {
+		fmt.Fprintf(buf, "%+v\n", f)
+	}
+	return []byte(strings.ReplaceAll(buf.String(), "\n\t", "()\n\t"))
+}
+
+func (this stackTraceError) Trace() []byte {
+	return this.trace
+}
+
 func New(errOrMsg interface{}, trace ...[]byte) TracedError {
 	var err error
 	switch t := errOrMsg.(type) {
+	case TracedError:
+		return t
 	case string:
 		err = errors.New(t)
+	case interface {
+		error
+		SimpleStackTracer
+	}:
+		var trace []byte
+		var te error
+
+		if c, ok := t.(Causer); ok {
+			te = c.Cause()
+		}
+
+	do:
+		for te != nil {
+			if c, ok := te.(Causer); ok {
+				switch t2 := c.(type) {
+				case TracedError:
+					trace = t2.Trace()
+					break do
+				case Causer:
+					te = t2.Cause()
+				case SimpleStackTracer:
+					trace = stackTracerToBytes(t2.(SimpleStackTracer))
+					break do
+				default:
+					break do
+				}
+			} else {
+				break
+			}
+		}
+		if trace == nil {
+			trace = stackTracerToBytes(t.(SimpleStackTracer))
+		}
+		te2 := &stackTraceError{t, trace}
+		return te2
+	case Causer:
+		if te, ok := t.Cause().(TracedError); ok {
+			trace = [][]byte{te.Trace()}
+		}
 	case error:
 		err = t
 	default:
